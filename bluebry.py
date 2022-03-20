1eb60d82236482c618fc2038637c4fe0f962e95e
PaddeCraft <70023807+PaddeCraft@users.noreply.github.com>
2022-03-20 14:43:41 +0100

    initial commit

---
+++ bluebry.py
@@ -0,0 +1,35 @@
+import json
+from re import A
+from log4py.log4py import logger
+from log4py.timeGenerator import timeGenerator
+import os
+import typer
+from typing import Tuple
+
+app = typer.Typer()
+
+__version__ = "1.0.0"
+
+blueberryPath = os.path.join(os.path.expanduser("~"), ".blueberry")
+
+os.makedirs(os.path.join(blueberryPath, "logs"), exist_ok=True)
+
+logFile = os.path.join(blueberryPath, "logs", timeGenerator("%Y%m%d-%H-%M-%S") + ".log")
+log = logger(timeFormat="%H:%M:%S", logFile=logFile)
+
+if os.path.isfile(os.path.join(blueberryPath, "LOCKFILE")):
+    log.warn("Blueberry is already running. Cannot invoke 2nd instance.")
+    log.warn("If this isn´t the case, delete the file '~/.blueberry/LOCKFILE'")
+    log.error("Failed to launch, quitting...")
+    exit(1)
+
+@app.command()
+def update():
+    pass
+
+@app.command()
+def install(pkgstr: str):
+    pkgs = pkgstr.split(",")
+
+if __name__ == '__main__':
+    app()
